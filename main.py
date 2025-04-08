b86c0506c87d65e390eb90e13be8d26b2a1156c2
FranziskoB <50852242+FranziskoB@users.noreply.github.com>
2025-04-07 22:19:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ from pydantic import BaseModel
 import pickle
 import numpy as np
 
-# Criação do app FastAPI
 app = FastAPI()
 
-# Carregar o modelo treinado
-with open("churn_model.pkl", "rb") as f:
+with open("model.pkl", "rb") as f:
     modelo = pickle.load(f)
 
-# Modelo dos dados de entrada
 class Entrada(BaseModel):
-    features: list[float]  # ou dict, depende do modelo
+    features: list[float]
 
 @app.post("/predict")
 def prever(dados: Entrada):
     X = np.array(dados.features).reshape(1, -1)
-    predicao = modelo.predict(X)
-    return {"predicao": predicao.tolist()}
+    predicao = modelo.predict(X).tolist()[0]
+    return {"features": dados.features, "predicao": predicao}
